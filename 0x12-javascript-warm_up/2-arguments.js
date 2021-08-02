36f01b41c05585e95edeb5f95ac3968ef85c6bc8
hamoudaabdrahmen <abdrahmenbenhamouda@gmail.com>
2021-08-02 22:38:14 +0100

    js warmup

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+
+
+    const len = process.argv.length;
+    if (len === 2)
+    {
+        console.log('No argument');
+    }
+    else if ( len ===3)
+    {
+        console.log('Argument found');
+    }
+    else{
+        console.log('Argument found');
+    }
